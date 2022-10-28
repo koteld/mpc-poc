21d14a05bc74cd4740eaaec66e108047c1d30c90
koteld <festbot@gmail.com>
2022-10-28 21:55:11 +0200

    update main/API module

--- main.go
+++ main.go
@@ -4,25 +4,30 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"mpc_poc/service"
 
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gorilla/mux"
 	"github.com/koteld/multi-party-sig/pkg/party"
 )
 
+var ids party.IDSlice
+
 type Parameters struct {
-	IDs       party.IDSlice `json:"ids"`
-	Threshold int           `json:"threshold"`
-	Message   string        `json:"message"`
+	Threshold int    `json:"threshold"`
+	Message   string `json:"message"`
+	Amount    string `json:"amount"`
+	To        string `json:"to"`
 }
 
 func GenerateKeys(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var parameters Parameters
 	_ = json.NewDecoder(r.Body).Decode(&parameters)
-	service.GenerateKeys(parameters.IDs, parameters.Threshold)
-	_ = json.NewEncoder(w).Encode("Distributed keys were generated successfully")
+	res := service.GenerateKeys(ids, parameters.Threshold)
+	_ = json.NewEncoder(w).Encode(res)
 }
 
 func RefreshKeys(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +35,7 @@ func RefreshKeys(w http.ResponseWriter, r *http.Request) {
 	var parameters Parameters
 	_ = json.NewDecoder(r.Body).Decode(&parameters)
 
-	service.RefreshKeys(parameters.IDs, parameters.Threshold)
+	service.RefreshKeys(ids, parameters.Threshold)
 	_ = json.NewEncoder(w).Encode("Distributed keys were refreshed successfully")
 }
 
@@ -38,15 +43,15 @@ func Sign(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var parameters Parameters
 	_ = json.NewDecoder(r.Body).Decode(&parameters)
-	service.Sign(parameters.IDs, parameters.Threshold, parameters.Message)
-	_ = json.NewEncoder(w).Encode("Message was signed successfully")
+	res := service.Sign(ids, parameters.Threshold, parameters.Message)
+	_ = json.NewEncoder(w).Encode(res)
 }
 
 func PreSign(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var parameters Parameters
 	_ = json.NewDecoder(r.Body).Decode(&parameters)
-	service.PreSign(parameters.IDs)
+	service.PreSign(ids)
 	_ = json.NewEncoder(w).Encode("Pre-signature was created successfully")
 }
 
@@ -54,8 +59,29 @@ func SignOnline(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var parameters Parameters
 	_ = json.NewDecoder(r.Body).Decode(&parameters)
-	service.SignOnline(parameters.IDs, parameters.Message)
-	_ = json.NewEncoder(w).Encode("Message was signed online successfully")
+	messageHash := crypto.Keccak256Hash([]byte(parameters.Message))
+	res := service.SignOnline(ids, messageHash)
+	_ = json.NewEncoder(w).Encode(res)
+}
+
+func SendEth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var parameters Parameters
+	_ = json.NewDecoder(r.Body).Decode(&parameters)
+	txHash := service.SendEth(ids)
+	_ = json.NewEncoder(w).Encode(txHash)
+}
+
+func GetOnline(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	online := service.GetOnline(ids)
+	_ = json.NewEncoder(w).Encode(online)
+}
+
+func GetConfigs(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	configs := service.GetConfigs(ids)
+	_ = json.NewEncoder(w).Encode(configs)
 }
 
 func initializeRouter() {
@@ -66,10 +92,20 @@ func initializeRouter() {
 	r.HandleFunc("/sign", Sign).Methods("POST")
 	r.HandleFunc("/presign", PreSign).Methods("POST")
 	r.HandleFunc("/signonline", SignOnline).Methods("POST")
+	r.HandleFunc("/sendeth", SendEth).Methods("POST")
+
+	r.HandleFunc("/online", GetOnline).Methods("GET")
+	r.HandleFunc("/configs", GetConfigs).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func main() {
+	idsArray := make([]party.ID, 0)
+	for _, id := range os.Args[1:] {
+		idsArray = append(idsArray, party.ID(id))
+	}
+	ids = party.NewIDSlice(idsArray)
+
 	initializeRouter()
 }
