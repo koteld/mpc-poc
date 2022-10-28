3c30b9f458eb61d99c12016043432212afe313b1
koteld <festbot@gmail.com>
2022-10-28 21:54:33 +0200

    update participant module

--- participant/participant.go
+++ participant/participant.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	b64 "encoding/base64"
 	"os"
 
+	"mpc_poc/helper"
 	"mpc_poc/models"
 	"mpc_poc/session"
 
@@ -17,107 +19,198 @@ import (
 	"github.com/koteld/multi-party-sig/protocols/cmp"
 )
 
-var config *cmp.Config
-var preSignature *ecdsa.PreSignature
-
 var ID party.ID
+var configs = make(map[string]*cmp.Config)
+var preSignatures = make(map[string]*ecdsa.PreSignature)
 
-func StartDKGProtocol(id party.ID, ids party.IDSlice, threshold int, sessionID []byte, pl *pool.Pool) {
-	h, _ := protocol.NewMultiHandler(cmp.Keygen(curve.Secp256k1{}, id, ids, threshold, pl), sessionID)
-	session.Loop(id, ids, h)
+func saveConfigurationToFile(address string, config *cmp.Config) {
+	wd, _ := os.Getwd()
 
-	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), id)
-	r, err := h.Result()
-	sessionMessage := models.SessionMessage{
-		Result: r,
-		Error:  err,
+	directory := wd + "/" + "participant-" + string(ID)
+	if !helper.PathExists(directory) {
+		_ = os.Mkdir(directory, os.ModePerm)
 	}
-	sessionMessageOutput <- &sessionMessage
 
-	config = r.(*cmp.Config)
+	filepath := directory + "/" + address
+	if helper.PathExists(filepath) {
+		_ = os.Remove(filepath)
+	}
+
+	configBytes, _ := config.MarshalBinary()
+	_ = os.WriteFile(filepath, configBytes, os.ModePerm)
 }
 
-func StartDKFProtocol(id party.ID, ids party.IDSlice, sessionID []byte, pl *pool.Pool) {
-	h, _ := protocol.NewMultiHandler(cmp.Refresh(config, pl), sessionID)
-	session.Loop(id, ids, h)
+func readConfigurationsFromFiles() {
+	dir, _ := os.Getwd()
+	filepath := dir + "/" + "participant-" + string(ID)
+	files, _ := os.ReadDir(filepath)
 
-	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), id)
-	r, err := h.Result()
-	sessionMessage := models.SessionMessage{
-		Result: r,
-		Error:  err,
+	for _, file := range files {
+		config := cmp.EmptyConfig(curve.Secp256k1{})
+		fileData, _ := os.ReadFile(filepath + "/" + file.Name())
+		_ = config.UnmarshalBinary(fileData)
+
+		publicKeyBytes, _ := config.PublicPoint().MarshalBinaryEth()
+		address := common.BytesToAddress(crypto.Keccak256(publicKeyBytes[1:])[12:])
+
+		configs[address.String()] = config
 	}
-	sessionMessageOutput <- &sessionMessage
-
-	config = r.(*cmp.Config)
 }
 
-func StartSignProtocol(id party.ID, ids party.IDSlice, messageHash []byte, sessionID []byte, pl *pool.Pool) {
-	h, _ := protocol.NewMultiHandler(cmp.Sign(config, ids, messageHash, pl), sessionID)
-	session.Loop(id, ids, h)
+func startDKGProtocol(ids party.IDSlice, threshold int, sessionID []byte, pl *pool.Pool) {
+	h, _ := protocol.NewMultiHandler(cmp.Keygen(curve.Secp256k1{}, ID, ids, threshold, pl), sessionID)
+	session.Loop(ID, ids, h)
 
-	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), id)
+	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), ID)
 	r, err := h.Result()
+
+	config := r.(*cmp.Config)
+	publicKeyBytes, _ := config.PublicPoint().MarshalBinaryEth()
+	address := common.BytesToAddress(crypto.Keccak256(publicKeyBytes[1:])[12:])
+
+	saveConfigurationToFile(address.String(), config)
+	configs[address.String()] = config
+
 	sessionMessage := models.SessionMessage{
-		Result: r,
-		Error:  err,
-	}
-	sessionMessageOutput <- &sessionMessage
-}
-
-func StartPreSignProtocol(id party.ID, ids party.IDSlice, sessionID []byte, pl *pool.Pool) {
-	h, _ := protocol.NewMultiHandler(cmp.Presign(config, ids, pl), sessionID)
-	session.Loop(id, ids, h)
-
-	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), id)
-	r, err := h.Result()
-	sessionMessage := models.SessionMessage{
-		Result: r,
-		Error:  err,
-	}
-	sessionMessageOutput <- &sessionMessage
-
-	preSignature = r.(*ecdsa.PreSignature)
-}
-
-func StartSignOnlineProtocol(id party.ID, ids party.IDSlice, messageHash []byte, sessionID []byte, pl *pool.Pool) {
-	h, _ := protocol.NewMultiHandler(cmp.PresignOnline(config, preSignature, messageHash, pl), sessionID)
-	session.Loop(id, ids, h)
-
-	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), id)
-	r, err := h.Result()
-	sessionMessage := models.SessionMessage{
-		Result: r,
+		Result: b64.StdEncoding.EncodeToString(publicKeyBytes),
 		Error:  err,
 	}
 	sessionMessageOutput <- &sessionMessage
 }
 
-func StartProtocol(message *models.ProtocolMessage) {
+func startDKFProtocol(address string, ids party.IDSlice, sessionID []byte, pl *pool.Pool) {
+	h, _ := protocol.NewMultiHandler(cmp.Refresh(configs[address], pl), sessionID)
+	session.Loop(ID, ids, h)
+
+	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), ID)
+	r, err := h.Result()
+
+	config := r.(*cmp.Config)
+
+	saveConfigurationToFile(address, config)
+	configs[address] = config
+
+	sessionMessage := models.SessionMessage{
+		Result: r,
+		Error:  err,
+	}
+	sessionMessageOutput <- &sessionMessage
+}
+
+func startSignProtocol(address string, ids party.IDSlice, messageHash []byte, sessionID []byte, pl *pool.Pool) {
+	h, _ := protocol.NewMultiHandler(cmp.Sign(configs[address], ids, messageHash, pl), sessionID)
+	session.Loop(ID, ids, h)
+
+	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), ID)
+	r, err := h.Result()
+	signature := r.(*ecdsa.Signature)
+	signatureCompact := signature.ToCompactEth()
+	sessionMessage := models.SessionMessage{
+		Result: b64.StdEncoding.EncodeToString(signatureCompact),
+		Error:  err,
+	}
+	sessionMessageOutput <- &sessionMessage
+}
+
+func startPreSignProtocol(address string, ids party.IDSlice, sessionID []byte, pl *pool.Pool) {
+	h, _ := protocol.NewMultiHandler(cmp.Presign(configs[address], ids, pl), sessionID)
+	session.Loop(ID, ids, h)
+
+	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), ID)
+	r, err := h.Result()
+
+	preSignatures[address] = r.(*ecdsa.PreSignature)
+
+	sessionMessage := models.SessionMessage{
+		Result: r,
+		Error:  err,
+	}
+	sessionMessageOutput <- &sessionMessage
+}
+
+func startSignOnlineProtocol(address string, ids party.IDSlice, messageHash []byte, sessionID []byte, pl *pool.Pool) {
+	h, _ := protocol.NewMultiHandler(cmp.PresignOnline(configs[address], preSignatures[address], messageHash, pl), sessionID)
+	session.Loop(ID, ids, h)
+
+	sessionMessageOutput := models.GetSessionMessageOutputChannel(string(sessionID), ID)
+	r, err := h.Result()
+	signature := r.(*ecdsa.Signature)
+	signatureCompact := signature.ToCompactEth()
+	sessionMessage := models.SessionMessage{
+		Result: b64.StdEncoding.EncodeToString(signatureCompact),
+		Error:  err,
+	}
+	sessionMessageOutput <- &sessionMessage
+}
+
+func startProtocol(message *models.ProtocolMessage) {
 	pl := pool.NewPool(0)
 	defer pl.TearDown()
 
 	switch message.Protocol {
 	case models.DKG:
-		StartDKGProtocol(ID, message.IDs, message.Threshold, message.SessionID, pl)
+		startDKGProtocol(message.IDs, message.Threshold, message.SessionID, pl)
 	case models.DKF:
-		StartDKFProtocol(ID, message.IDs, message.SessionID, pl)
+		startDKFProtocol(message.Address, message.IDs, message.SessionID, pl)
 	case models.Sign:
-		StartSignProtocol(ID, message.IDs, message.MessageHash, message.SessionID, pl)
+		startSignProtocol(message.Address, message.IDs, message.MessageHash, message.SessionID, pl)
 	case models.PreSign:
-		StartPreSignProtocol(ID, message.IDs, message.SessionID, pl)
+		startPreSignProtocol(message.Address, message.IDs, message.SessionID, pl)
 	case models.SignOnline:
-		StartSignOnlineProtocol(ID, message.IDs, message.MessageHash, message.SessionID, pl)
+		startSignOnlineProtocol(message.Address, message.IDs, message.MessageHash, message.SessionID, pl)
 	}
 }
 
-func Activate(_ context.Context) {
+func getOnline() {
+	infoMessageOutput := models.GetInfoResponseMessageOutputChannel(ID)
+	infoMessage := models.InfoResponseMessage{
+		Info:   models.Online,
+		Online: true,
+	}
+	infoMessageOutput <- &infoMessage
+}
+
+func getConfigs() {
+	infoMessageOutput := models.GetInfoResponseMessageOutputChannel(ID)
+	configMessages := make([]models.ConfigMessage, 0, len(configs))
+	for address, config := range configs {
+		configMessage := models.ConfigMessage{
+			Address: address,
+			IDs:     config.PartyIDs(),
+		}
+		configMessages = append(configMessages, configMessage)
+	}
+	infoMessage := models.InfoResponseMessage{
+		Info:    models.Online,
+		Configs: configMessages,
+	}
+	infoMessageOutput <- &infoMessage
+}
+
+func getInfo(message *models.InfoRequestMessage) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+
+	switch message.Info {
+	case models.Online:
+		getOnline()
+	case models.Configs:
+		getConfigs()
+	}
+}
+
+func activate(_ context.Context) {
+	readConfigurationsFromFiles()
+
+	infoMessageInput := models.GetInfoRequestMessageInputChannel(ID)
 	protocolMessageInput := models.GetProtocolMessageInputChannel(ID)
 
 	for {
 		select {
+		case infoMessage := <-infoMessageInput:
+			getInfo(infoMessage)
 		case protocolMessage := <-protocolMessageInput:
-			StartProtocol(protocolMessage)
+			startProtocol(protocolMessage)
 		}
 	}
 }
@@ -125,5 +218,5 @@ func Activate(_ context.Context) {
 func main() {
 	ctx := context.Background()
 	ID = party.ID(os.Args[1])
-	Activate(ctx)
+	activate(ctx)
 }
