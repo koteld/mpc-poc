75e48faeac92dd7e54ae4c04d02ed7480e7a4cf8
koteld <festbot@gmail.com>
2022-11-04 00:17:30 +0100

    add log messages channels

---
+++ mpc-application/models/logMessages.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"encoding/json"
+
+	"mpc_poc/messaging"
+)
+
+type (
+	LogMessage struct {
+		SessionID   string   `json:"sessionID"`
+		Participant string   `json:"participant"`
+		Protocol    Protocol `json:"protocol"`
+		Round       uint16   `json:"round"`
+		Message     string   `json:"message"`
+		Timestamp   string   `json:"timestamp"`
+	}
+)
+
+func GetLogMessageInputChannel() <-chan *LogMessage {
+	rawInput := messaging.GetInputChannel(messaging.LogMessagesChannel)
+	res := make(chan *LogMessage)
+
+	go func() {
+		for val := range rawInput {
+			bs := &LogMessage{}
+			err := json.Unmarshal(val, bs)
+			if err == nil {
+				res <- bs
+			}
+		}
+	}()
+
+	return res
+}
+
+func GetLogMessageOutputChannel() chan<- *LogMessage {
+	rawOutput := messaging.GetOutputChannel(messaging.LogMessagesChannel)
+	res := make(chan *LogMessage)
+
+	go func() {
+		for bs := range res {
+			val, err := json.Marshal(bs)
+			if err == nil {
+				rawOutput <- val
+			}
+		}
+	}()
+
+	return res
+}
